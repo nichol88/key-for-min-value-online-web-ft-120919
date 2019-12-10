aa9d1e635179b06964a8b6fa5edcbf9159d1576c
Brian Nicholls <nichol88@gmail.com>
2019-12-10 23:40:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  inject (0) { |memo, item| 
+  inject (0) { |memo, item|
     if item > memo
       memo = item
     end
