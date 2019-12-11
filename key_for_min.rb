1105eb229c4e8d111821c70d4220e2b70bf9da2e
Brian Nicholls <nichol88@gmail.com>
2019-12-11 00:04:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,13 @@
 
 def key_for_min_value(name_hash)
   r = nil
+  if !name_hash.empty?
   name_hash.inject(0){ |memo, k, v|
     if v < memo || r == nil
       r = k
     else
     end
   }
+end
 r
 end
